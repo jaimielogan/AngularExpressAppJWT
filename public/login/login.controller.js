dd28b7c339deebabeb0d9e9f18e5aa6dd499f18a
jaimielogan <jaimielogan@gmail.com>
2016-10-17 18:15:18 -0600

    Login controller created with partial jwt logic

--- public/login/login.controller.js
+++ public/login/login.controller.js
@@ -1,5 +1,5 @@
 angular.module('angularExpress')
-.controller('login', function($scope, $http){
+.controller('login', function($scope, $http, $location){
   $scope.createUser = function(){
     var user = {
       username: $scope.username,
@@ -8,12 +8,13 @@ angular.module('angularExpress')
 
     $http.post('/users/create', user)
     .then(function(result){
-      console.log(result);
+      localStorage.setItem('user', JSON.stringify(result.data));
+
+      $location.path('/dashboard');
     })
     .catch(function(error){
       console.error(error);
     });
-
   };
 
 });
